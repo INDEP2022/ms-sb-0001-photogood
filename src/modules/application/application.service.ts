7eb3d309a6b825546148431164c60c38cf431f95
Yeltsin <yeltsinrhp.93@gmail.com>
2023-05-31 12:07:57 -0500

    FEAT FCONGENRASTREADOR

--- src/modules/application/application.service.ts
+++ src/modules/application/application.service.ts
@@ -89,4 +89,101 @@ export class ApplicationService {
             };
         }
     }
+
+    async pupExportPhotosGood() {
+        try {
+            const result = await this.entity.query(`
+            SELECT  CONCAT('"', RUTA, '*.*"') AS PATH_ORIGEN, NO_BIEN
+            FROM (
+                SELECT NO_BIEN, LEFT(UBICACION, POSITION('/' IN REVERSE(UBICACION)) - 1) AS RUTA
+                FROM sera.BIENES_FOTO BT
+                WHERE NO_BIEN IN (SELECT NO_BIEN FROM sera.TMP_BIEN_FOTO)
+            ) AS subquery
+            GROUP BY RUTA, NO_BIEN;`)
+
+            return {
+                statusCode: HttpStatus.OK,
+                message: ['Busqueda existosa'],
+                data: result
+            };
+        } catch (error) {
+            return {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: [error.message],
+            };
+        }
+    }
+
+    async pupExportPhotosMasiv(id) {
+        try {
+            const result = await this.entity.query(`
+            SELECT '"' || RUTA || '*.jpg"' AS PATH_ORIGEN, NO_BIEN
+            FROM (
+                SELECT NO_BIEN, LEFT(UBICACION, POSITION('/' IN REVERSE(UBICACION)) - 1) AS RUTA
+                FROM sera.BIENES_FOTO BT
+                WHERE NO_BIEN = ${id}				
+            ) AS subquery
+            GROUP BY RUTA, NO_BIEN;`)
+
+            return {
+                statusCode: HttpStatus.OK,
+                message: ['Busqueda existosa'],
+                data: result
+            };
+        } catch (error) {
+            return {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: [error.message],
+            };
+        }
+    }
+
+    async pupInsListPhoto() {
+        try {
+            const result = await this.entity.query(`
+            SELECT UBICACION
+            FROM sera.BIENES_FOTO BF
+            WHERE NOT EXISTS (
+                SELECT 1
+                FROM siabinfo.BIENES_FOTO_INVALIDAS BFI
+                WHERE BFI.NO_BIEN = BF.NO_BIEN
+                AND BFI.NO_CONSEC = BF.NO_CONSEC
+            );`)
+            return {
+                statusCode: HttpStatus.OK,
+                message: ['Busqueda existosa'],
+                data: result
+            };
+        } catch (error) {
+            return {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: [error.message],
+            };
+        }
+    }
+
+    async pupGetsPhotos() {
+        try {
+            const result = await this.entity.query(`
+            SELECT CONCAT('"', RUTA, '*.*"') AS PATH_ORIGEN, CONCAT('"C:\\\\SIABTMP\\\\BIENES_FOTO\\\\', NO_BIEN, '"\\ /Y /S /D /C /F') AS PATH_DESTINO
+            FROM (
+                SELECT NO_BIEN, SPLIT_PART(UBICACION, '\\\\', 1) || '\\\\' || SPLIT_PART(UBICACION, '\\\\', 2) || '\\\\' AS RUTA  
+                FROM sera.BIENES_FOTO
+                WHERE NO_BIEN IN (SELECT NO_BIEN
+                                FROM sera.TMP_BIEN_FOTO)                                  
+            ) AS subquery
+            GROUP BY NO_BIEN, RUTA;`)
+        
+            return {
+                statusCode: HttpStatus.OK,
+                message: ['Busqueda existosa'],
+                data: result
+            };
+        } catch (error) {
+            return {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: [error.message],
+            };
+        }
+    }
 }
