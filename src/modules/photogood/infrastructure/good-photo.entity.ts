de05295adb9ec46ee80ede3d3e71c048197ef6fa
Edwin1198 <edwin.apd.1198@gmail.com>
2023-08-16 11:11:41 -0500

    fix

--- src/modules/photogood/infrastructure/good-photo.entity.ts
+++ src/modules/photogood/infrastructure/good-photo.entity.ts
@@ -4,16 +4,12 @@ export class GoodPhotoEntity {
   @PrimaryColumn({
     type: 'numeric',
     name: 'no_bien',
-
-    precision: 10,
   })
   goodNumber: number;
 
   @PrimaryColumn({
     type: 'numeric',
     name: 'no_consec',
-
-    precision: 10,
   })
   consecNumber: number;
 
@@ -47,24 +43,18 @@ export class GoodPhotoEntity {
   @Column({
     type: 'numeric',
     name: 'publ_img_cat_web',
-
-    precision: 16,
   })
   publicImgcatWeb: number;
 
   @Column({
     type: 'numeric',
     name: 'existe_fs',
-
-    precision: 16,
   })
   existsfs: number;
 
   @Column({
     type: 'numeric',
     name: 'existe_prod',
-
-    precision: 16,
   })
   existsproduction: number;
 }
